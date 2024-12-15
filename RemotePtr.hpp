3b4171d6d7e7de38d2f9bd0736cc8efe0fe1aab9
abel1502 <terragame.abel@gmail.com>
2024-12-16 01:02:38 +0300

    Finish RemotePtr (?)

--- RemotePtr.hpp
+++ RemotePtr.hpp
@@ -14,15 +14,15 @@ template <typename T = void>
 class RemotePtr {
 protected:
     Handle process{};
-    T *value = nullptr;
+    T *ptr = nullptr;
 
 public:
     RemotePtr() noexcept = default;
     RemotePtr(nullptr_t) noexcept :
         RemotePtr() {
     }
-    RemotePtr(Handle process, T *value) noexcept :
-        process(process), ptr(value) {
+    RemotePtr(Handle process, T *ptr) noexcept :
+        process(process), ptr(ptr) {
     }
 
     RemotePtr(const RemotePtr &other) noexcept = default;
@@ -39,22 +39,98 @@ public:
     }
 
     operator bool() const noexcept {
-        return process && value;
+        return process && ptr;
     }
 
-    T read() requires !std::is_void<T> const {
+    T read() /*requires !std::is_void<T>*/ const {
+        T result{};
+        bool success = ReadProcessMemory(
+            process,
+            ptr,
+            &result,
+            sizeof(T),
+            nullptr
+        );
+        if (!success) {
+            fail("Failed to read process memory");
+        }
+        return result;
     }
 
-    void write(T value) requires !std::is_void<T> const {
+    void write(const T &value) /*requires !std::is_void<T>*/ const {
+        bool success = WriteProcessMemory(
+            process,
+            ptr,
+            &value,
+            sizeof(T),
+            nullptr
+        );
+        if (!success) {
+            fail("Failed to write process memory");
+        }
+        return result;
     }
 
     template <typename U>
     RemotePtr<U> cast() const {
-        return {process, (U *)(value)};
+        return {process, (U *)(ptr)};
     }
 
-    // TODO: Pointer arithmetic: add/sub size_t, sub RemotePtr<T>, custom case for void
-    // TODO: Convenience wrappers: dereferencing proxy, cast operators
+    RemotePtr<T> operator+(size_t offset) const {
+        return {process, ptr + offset};
+    }
+
+    RemotePtr<T> operator-(size_t offset) const {
+        return {process, ptr - offset};
+    }
+
+    RemotePtr<T> &operator+=(size_t offset) {
+        ptr += offset;
+        return *this;
+    }
+
+    RemotePtr<T> &operator-=(size_t offset) {
+        ptr -= offset;
+        return *this;
+    }
+
+    RemotePtr<T> &operator++() {
+        ++ptr;
+        return *this;
+    }
+
+    RemotePtr<T> &operator--() {
+        --ptr;
+        return *this;
+    }
+
+    struct reference_type {
+        const RemotePtr<T> &ptr;
+
+        reference_type(RemotePtr<T> &ptr) :
+            ptr(ptr) {
+        }
+
+        reference_type(const reference_type &other) = default;
+        reference_type &operator=(const reference_type &other) = default;
+
+        operator T() {
+            return ptr.read();
+        }
+
+        T operator=(const T &value) {
+            ptr.write(value);
+            return value;
+        }
+    };
+
+    reference_type operator*() const {
+        return {*this};
+    }
+
+    reference_type operator->() const {
+        return {*this};
+    }
 };
 
 }  // namespace abel
